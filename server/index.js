31c9cdfb6eee9493ddb1396f5c31590aa3510caa
fabrizio <fabrizio.dimarco@gmail.com>
2023-04-23 18:54:28 +0200

    testing deploy

--- server/index.js
+++ server/index.js
@@ -45,7 +45,8 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
+// app.post("/posts", verifyToken, upload.single("picture"), createPost);
+app.post("/posts", upload.single("picture"), createPost); //for testing purposes
 
 /* ROUTES */
 app.use("/auth", authRoutes);
