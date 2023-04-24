675bdcba87e8b5d24a1e2f6c4fc28402231a05a3
fabrizio <fabrizio.dimarco@gmail.com>
2023-04-24 17:23:34 +0200

    backtoverifytoken

--- server/index.js
+++ server/index.js
@@ -45,8 +45,8 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-// app.post("/posts", verifyToken, upload.single("picture"), createPost);
-app.post("/posts", upload.single("picture"), createPost); //for testing purposes
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
 
 /* ROUTES */
 app.use("/auth", authRoutes);
